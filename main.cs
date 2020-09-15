98d2812e8fe7e5835cac98d61c53d076273f1c9a
Jeff <jeckman03@gmail.com>
2020-09-15 21:50:44 +0000

    wrote the layout of the program as comments

--- main.cs
+++ main.cs
@@ -2,7 +2,29 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    
+
+    //-----Create a to do list application-----
+
+    //display Welcome the the application
+
+    //display the following options the user can do. Add to the list, remove an item from the list, view the list, or exit the application
+
+    //add to the list
+      //ask the user to enter a task to add
+        //comfirm task has been added
+
+    //remove a task 
+      //display task list
+      //ask what task they want to remove
+        //remove from list
+      //confirm task has been removed
+
+    //view the list
+      //display the current to do's on the list
+
+    //exit the application
+      //terminate the program          
+
     Console.ReadLine();
   }
 }
